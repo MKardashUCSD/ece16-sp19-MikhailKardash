f817279a7fc2640a381fd8bdfd90bea555839402
MKardashUCSD <49382433+MKardashUCSD@users.noreply.github.com>
2019-05-08 12:26:39 -0700

    Add files via upload

---
+++ Lab5/Obj1/ReadingAnalogSignal/ReadingAnalogSignal.ino
@@ -0,0 +1,26 @@
+int sampleRate;
+int reading;
+float frequency;
+long int prevTime;
+
+
+void setup() {
+  // put your setup code here, to run once:
+  pinMode(A1,INPUT);
+  sampleRate = 10;
+  reading;
+  frequency = (float) 1/sampleRate;
+  prevTime = millis();
+  Serial.begin(9600);
+  
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  if(millis() - prevTime > sampleRate) {
+    prevTime = millis();
+    reading = analogRead(A1);
+    Serial.println(reading);
+  }
+
+}
